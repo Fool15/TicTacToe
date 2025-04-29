3096e773a16996d4da6e07d7e40188ef04402481
Flavio <flaviodervishi239@gmail.com>
2025-04-29 21:18:00 +0200

    Loja ndalon dhe kur boti fiton

--- Game/src/1PlayerPage/OnePlayerPage.jsx
+++ Game/src/1PlayerPage/OnePlayerPage.jsx
@@ -10,7 +10,6 @@ function OnePlayerPage() {
   let [winner, SetWinner] = useState("");
   let [draw, setDraw] = useState("");
 
-
   // function turnSwitcher() {
   //   if (WhosTurn == true) {
   //     SetTurn(false);
@@ -21,110 +20,158 @@ function OnePlayerPage() {
   //   }
   // }
 
-    let allMoves=[0,1,2,3,4,5,6,7,8]
- 
-  function callOnClick(e,position) {
-    let buttons = document.querySelectorAll(".baseStyle");
-    
-    function shapeDrawn(e,position) {
+  let allMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
+  function callOnClick(e, position) {
+    let buttons = document.querySelectorAll(".baseStyle");
+
+    function shapeDrawn(e, position) {
+      winCondition();
       let targetContent = e.target;
-      
-      
-      if (hasWon == false && targetContent.textContent == "") {
-        targetContent.textContent = `${WhosTurn?"X":"O"}`;
-        
-        console.log("my move",position)
-        allMoves[position]=undefined
+      if (hasWon) return;
+
+      if (targetContent.textContent === "") {
+        targetContent.textContent = `${WhosTurn ? "X" : "O"}`;
+
+        console.log("my move", position);
+        allMoves[position] = undefined;
       }
 
-      let movesAvaliable=allMoves.filter((nr)=>{
-        return nr!=undefined
-       })
-       
-       console.log("all avaliable moves",movesAvaliable)
+      let movesAvaliable = allMoves.filter((nr) => {
+        return nr != undefined;
+      });
 
-       let randomMove=Math.floor(Math.random()*movesAvaliable.length) //[1,2,3,4,5,6,7,8]
-       console.log("robots move",randomMove)
+      console.log("all avaliable moves", movesAvaliable);
 
-      if(hasWon == false){
-        setTimeout(()=>{
-          buttons[movesAvaliable[randomMove]].textContent=`${!WhosTurn?"X":"O"}`
+      let randomMove = Math.floor(Math.random() * movesAvaliable.length); //[1,2,3,4,5,6,7,8]
+      if (hasWon == false) {
+        setTimeout(() => {
+          buttons[movesAvaliable[randomMove]].textContent = `${
+            !WhosTurn ? "X" : "O"
+          }`;
 
-          for(let i=0;i<allMoves.length;i++){
-            if(allMoves[i]==movesAvaliable[randomMove]){
-              allMoves[i]=undefined
+          for (let i = 0; i < allMoves.length; i++) {
+            if (allMoves[i] == movesAvaliable[randomMove]) {
+              allMoves[i] = undefined;
             }
           }
 
-        },500)
+          winCondition();
+        }, 500);
       }
-
     }
 
     function winCondition() {
-      let XorO = WhosTurn ? "X" : "O";
+      {
+        /*let XorO = WhosTurn ? "X" : "O";
 
       let onWin = () => {
-        SetWinner(`Player ${("X"||"O")} has won`);
+        SetWinner(`Player ${XorO} has won`);
 
         SetWon(true);
       };
 
-      console.log(("X"||"O"));
+      console.log("X" || "O");
 
-
-  
-      if (buttons[0].textContent == ("X"||"O") &&buttons[1].textContent == ("X"||"O") &&buttons[2].textContent == ("X"||"O")) {
+      if (
+        buttons[0].textContent == ("X" || "O") &&
+        buttons[1].textContent == ("X" || "O") &&
+        buttons[2].textContent == ("X" || "O")
+      ) {
         onWin();
-      } 
-      else if (buttons[3].textContent == ("X"||"O") &&buttons[4].textContent == ("X"||"O") &&buttons[5].textContent == ("X"||"O")) {
+      } else if (
+        buttons[3].textContent == ("X" || "O") &&
+        buttons[4].textContent == ("X" || "O") &&
+        buttons[5].textContent == ("X" || "O")
+      ) {
         onWin();
-      } 
-      else if (buttons[6].textContent == ("X"||"O") &&buttons[7].textContent == ("X"||"O") &&buttons[8].textContent == ("X"||"O")) {
+      } else if (
+        buttons[6].textContent == ("X" || "O") &&
+        buttons[7].textContent == ("X" || "O") &&
+        buttons[8].textContent == ("X" || "O")
+      ) {
         onWin();
-
-      } 
-      else if (buttons[0].textContent == ("X"||"O") &&buttons[3].textContent == ("X"||"O") &&buttons[6].textContent == ("X"||"O")) {
+      } else if (
+        buttons[0].textContent == ("X" || "O") &&
+        buttons[3].textContent == ("X" || "O") &&
+        buttons[6].textContent == ("X" || "O")
+      ) {
         onWin();
-      }
-      else if (buttons[1].textContent == ("X"||"O") &&buttons[4].textContent == ("X"||"O") &&buttons[7].textContent == ("X"||"O")) {
+      } else if (
+        buttons[1].textContent == ("X" || "O") &&
+        buttons[4].textContent == ("X" || "O") &&
+        buttons[7].textContent == ("X" || "O")
+      ) {
         onWin();
-      }
-      else if (buttons[2].textContent == ("X"||"O") &&buttons[5].textContent == ("X"||"O") &&buttons[8].textContent == ("X"||"O")) {
+      } else if (
+        buttons[2].textContent == ("X" || "O") &&
+        buttons[5].textContent == ("X" || "O") &&
+        buttons[8].textContent == ("X" || "O")
+      ) {
         onWin();
-      } 
-      else if (buttons[0].textContent == ("X"||"O") &&buttons[4].textContent == ("X"||"O") &&buttons[8].textContent == ("X"||"O")) {
+      } else if (
+        buttons[0].textContent == ("X" || "O") &&
+        buttons[4].textContent == ("X" || "O") &&
+        buttons[8].textContent == ("X" || "O")
+      ) {
         onWin();
-      } 
-      else if (buttons[6].textContent == ("X"||"O") &&buttons[4].textContent == ("X"||"O") &&buttons[2].textContent == ("X"||"O")) {
+      } else if (
+        buttons[6].textContent == ("X" || "O") &&
+        buttons[4].textContent == ("X" || "O") &&
+        buttons[2].textContent == ("X" || "O")
+      ) {
         onWin();
-      } 
-      
-      else {
-
+      } else {
         let isDraw = true;
 
         buttons.forEach((btn) => {
-
           if (btn.textContent === "") {
             isDraw = false;
           }
-
         });
 
         if (isDraw && !hasWon) {
           setDraw("It's a draw! Restart the game!!");
         }
+      } */
+        const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7], //kombinimet e mundshme
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ];
+        let buttons = document.querySelectorAll(".baseStyle");
+        for (let [a, b, c] of lines) {
+          const btnA = buttons[a].textContent;
+          const btnB = buttons[b].textContent; //merr tekstin ose vleren nga butoni  dhe e ruan
+          const btnC = buttons[c].textContent;
+          if (btnA !== "" && btnA === btnB && btnA === btnC) {
+            SetWinner(`Player ${btnA} has won`);
+            SetWon(true);
+            return;
+          } else {
+            let isDraw = true;
+            buttons.forEach((btn) => {
+              if (btn.textContent === "") {
+                isDraw = false;
+              }
+            });
+
+            if (isDraw && !hasWon) {
+              setDraw("It's a draw! Restart the game!!");
+            }
+          }
+        }
       }
     }
 
-    
-    shapeDrawn(e,position);
+    shapeDrawn(e, position);
     winCondition();
     // robotMove()
-
-
   }
 
   function restart() {
@@ -149,30 +196,79 @@ function OnePlayerPage() {
       <h1 style={{ color: "white" }}>{draw}</h1>
 
       <div className="gameContainer">
+        <button
+          className="baseStyle leftBorder topBorder"
+          onClick={(e) => {
+            callOnClick(e, 0);
+          }}
+        ></button>
+        <button
+          className="baseStyle rightBorder topBorder leftBorder "
+          onClick={(e) => {
+            callOnClick(e, 1);
+          }}
+        ></button>
+        <button
+          className="baseStyle rightBorder topBorder"
+          onClick={(e) => {
+            callOnClick(e, 2);
+          }}
+        ></button>
 
-        <button className="baseStyle leftBorder topBorder" onClick={(e) => {callOnClick(e,0);}}></button>
-        <button className="baseStyle rightBorder topBorder leftBorder " onClick={(e) => {callOnClick(e,1);}}></button>
-        <button className="baseStyle rightBorder topBorder" onClick={(e) => {callOnClick(e,2);}}></button>
+        <button
+          className="baseStyle leftBorder topBorder"
+          onClick={(e) => {
+            callOnClick(e, 3);
+          }}
+        ></button>
+        <button
+          className="baseStyle rightBorder topBorder leftBorder "
+          onClick={(e) => {
+            callOnClick(e, 4);
+          }}
+        ></button>
+        <button
+          className="baseStyle rightBorder topBorder"
+          onClick={(e) => {
+            callOnClick(e, 5);
+          }}
+        ></button>
 
-        <button className="baseStyle leftBorder topBorder" onClick={(e) => {callOnClick(e,3);}}></button>
-        <button className="baseStyle rightBorder topBorder leftBorder " onClick={(e) => {callOnClick(e,4);}}></button>
-        <button className="baseStyle rightBorder topBorder" onClick={(e) => {callOnClick(e,5);}}></button>
-
-
-        <button className="baseStyle  leftBorder topBorder bottomBorder" onClick={(e) => { callOnClick(e,6);}}></button>
-        <button className="baseStyle rightBorder topBorder leftBorder bottomBorder" onClick={(e) => {callOnClick(e,7); }} ></button>
-        <button className="baseStyle rightBorder topBorder  bottomBorder" onClick={(e) => {callOnClick(e,8);}}></button>
-
+        <button
+          className="baseStyle  leftBorder topBorder bottomBorder"
+          onClick={(e) => {
+            callOnClick(e, 6);
+          }}
+        ></button>
+        <button
+          className="baseStyle rightBorder topBorder leftBorder bottomBorder"
+          onClick={(e) => {
+            callOnClick(e, 7);
+          }}
+        ></button>
+        <button
+          className="baseStyle rightBorder topBorder  bottomBorder"
+          onClick={(e) => {
+            callOnClick(e, 8);
+          }}
+        ></button>
       </div>
 
       <br />
       <br />
 
       <div className="buttons">
-
-        <button className="buttonStyle" onClick={() => navigate("/")}>{"Home"}</button>
-        <button className="buttonStyle" onClick={() => { restart(); }}>Restart </button>
-
+        <button className="buttonStyle" onClick={() => navigate("/")}>
+          {"Home"}
+        </button>
+        <button
+          className="buttonStyle"
+          onClick={() => {
+            restart();
+          }}
+        >
+          Restart{" "}
+        </button>
       </div>
     </>
   );
